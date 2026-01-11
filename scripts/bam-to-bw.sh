82b3d75897e3a13b2870ef1fd3186d6a42edf51f
goldentree1 <goldentreedev@gmail.com>
2026-01-11 19:29:27 +1300

    fix bin size

--- scripts/bam-to-bw.sh
+++ scripts/bam-to-bw.sh
@@ -7,4 +7,4 @@
 # ./scripts/bam-to-bw.sh "public/data/LESB58/Pa_BF_s1Aligned.sortedByCoord.out.bam.reheader.bam" "bigwig.bw"
 
 samtools index "$1"
-bamCoverage --binSize 100 -b "$1" -o "$2"
+bamCoverage --binSize 5 -b "$1" -o "$2"
