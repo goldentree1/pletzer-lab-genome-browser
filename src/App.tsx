d433021e2b6ff716048b2f0ccba6326ed400b051
goldentree1 <goldentreedev@gmail.com>
2026-01-11 17:34:39 +1300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
+import type { ViewModel } from './types';
 import { useState, useEffect } from 'react';
+import { JBrowseLinearGenomeView } from '@jbrowse/react-linear-genome-view2';
 // @ts-expect-error no types for font
 import '@fontsource/roboto';
-import { JBrowseLinearGenomeView } from '@jbrowse/react-linear-genome-view2';
-import type { JBrowseConfig, ViewModel } from './types';
-
-import config_LESB58 from './config-LESB58';
-import config_GCF_000014625 from './config-GCF_000014625';
-import config_GCF_000006765_1 from './config-GCF_000006765.1';
-import config_GCF_000013425_1 from './config-GCF_000013425.1';
-import config_GCF_000013465_1 from './config-GCF_000013465.1';
-import config_GCF_000281535_2 from './config-GCF_000281535.2';
-import config_GCF_031932345_1 from './config-GCF_031932345.1';
+import myConf from './config';
+import { buildConfig } from './build-config';
 import { myCreateViewState } from './jbrowse-custom';
 
 function App() {
-  const datasetNames = [
-    'P.aeruginosa LESB58',
-    'P.aeruginosa PA14',
-    'P.aeruginosa PAO1',
-    'S.aureus HG001 (NCTC 8325)',
-    'S.aureus USA300LAC',
-    'K.pneumoniae KPNIH1',
-    'A.baumannii AB5075-UW',
-  ];
-  const [configName, setConfigName] = useState(datasetNames[0]);
+  const strainNames = Object.keys(myConf);
+
+  const [configName, setConfigName] = useState(strainNames[0]);
   const [viewState, setViewState] = useState<ViewModel>();
 
   // update view with selected dataset
   useEffect(() => {
-    const config = getConf(configName);
+    const config = myConf[configName];
     if (!config) {
       return;
     }
-    const state = myCreateViewState(config);
+    const state = myCreateViewState(buildConfig(config));
     setViewState(state);
     resetCheckboxes();
   }, [configName]);
@@ -50,7 +37,7 @@ function App() {
         </div>
         <nav className="header-genome-chooser">
           <select onChange={event => setConfigName(event.target.value)}>
-            {datasetNames.map(datasetName => (
+            {strainNames.map(datasetName => (
               <option value={datasetName} key={`dataset-${datasetName}`}>
                 {datasetName}
               </option>
@@ -124,26 +111,6 @@ function App() {
         }
       });
     });
-  }
-
-  function getConf(datasetName: string): JBrowseConfig | null {
-    if (datasetName === 'P.aeruginosa LESB58') {
-      return config_LESB58;
-    } else if (datasetName === 'P.aeruginosa PA14') {
-      return config_GCF_000014625;
-    } else if (datasetName === 'P.aeruginosa PAO1') {
-      return config_GCF_000006765_1;
-    } else if (datasetName === 'S.aureus HG001 (NCTC 8325)') {
-      return config_GCF_000013425_1;
-    } else if (datasetName === 'S.aureus USA300LAC') {
-      return config_GCF_000013465_1;
-    } else if (datasetName === 'K.pneumoniae KPNIH1') {
-      return config_GCF_000281535_2;
-    } else if (datasetName === 'A.baumannii AB5075-UW') {
-      return config_GCF_031932345_1;
-    } else {
-      return null;
-    }
   }
 
   function isWiggleDisplay(d: { type: string }) {
---
+++ src/config.ts
@@ -0,0 +1,145 @@
+import type { ConfigBuilderOpts } from './types';
+
+const myConfig: { [key: string]: ConfigBuilderOpts } = {
+  'P.aeruginosa PAO1': {
+    ncbiName: 'GCF_000006765.1',
+    firstRegion: 'NC_002516.2',
+    data: {
+      refSeq: 'refseq.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gff.gz',
+      coverage: [],
+    },
+  },
+  'S.aureus HG001 (NCTC 8325)': {
+    ncbiName: 'GCF_000013425.1',
+    firstRegion: 'NC_007795.1',
+    data: {
+      refSeq: 'refseq.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gz',
+      coverage: [],
+    },
+  },
+  'S.aureus USA300LAC': {
+    ncbiName: 'GCF_000013465.1',
+    firstRegion: 'NC_007793.1',
+    data: {
+      refSeq: 'refseq.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gff.gz',
+      coverage: [],
+    },
+  },
+  'P.aeruginosa PA14': {
+    ncbiName: 'GCF_000014625',
+    firstRegion: 'NC_008463.1',
+    data: {
+      refSeq: 'GCF_000014625.1_ASM1462v1_genomic.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gff.gz',
+      coverage: ['PA14_I_1.bw', 'PA14_Un_1.bw'],
+    },
+  },
+  'K.pneumoniae KPNIH1': {
+    ncbiName: 'GCF_000281535.2',
+    firstRegion: 'NZ_CP008827.1',
+    data: {
+      refSeq: 'refseq.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gff.gz',
+      coverage: [],
+    },
+  },
+  'A.baumannii AB5075-UW': {
+    ncbiName: 'GCF_031932345.1',
+    firstRegion: 'NZ_JAVSCP010000001.1',
+    data: {
+      refSeq: 'refseq.fna.gz',
+      genomic: 'genomic.gff.sorted.noregion.gff.gz',
+      coverage: [],
+    },
+  },
+  'P.aeruginosa LESB58': {
+    ncbiName: 'LESB58',
+    firstRegion: 'NC_011770.1',
+    data: {
+      refSeq: 'LESB58_ASM2664v1.fasta.gz',
+      genomic: 'LESB58_ASM2664v1.gff.sorted.gff.noregion.gz',
+      coverage: ['bigwig-bin5.bw', 'bigwig-bin100.bw'],
+    },
+    extras: [
+      // temporary - extras may be useful
+      {
+        type: 'VariantTrack',
+        trackId: 'VcfVariantTrack1',
+        name: 'Variants',
+        assemblyNames: ['asm'],
+        adapter: {
+          type: 'VcfTabixAdapter',
+          vcfGzLocation: {
+            uri: '/data/LESB58/outputCorrected.vcf.gz',
+            locationType: 'UriLocation',
+          },
+          index: {
+            location: {
+              uri: '/data/LESB58/outputCorrected.vcf.gz.tbi',
+              locationType: 'UriLocation',
+            },
+          },
+        },
+      },
+      {
+        type: 'QuantitativeTrack',
+        trackId: 'BigWig-bin5',
+        name: 'Coverage bin5',
+        assemblyNames: ['asm'],
+        adapter: {
+          type: 'BigWigAdapter',
+          bigWigLocation: {
+            uri: '/data/LESB58/bigwig-bin5.bw',
+            locationType: 'UriLocation',
+          },
+        },
+        displays: [
+          {
+            type: 'LinearWiggleDisplay',
+            displayId: 'BigWig-bin5-LinearWiggleDisplay',
+            height: 150,
+            renderer: {
+              type: 'XYPlotRenderer',
+              color: '#1f77b4',
+              lineWidth: 1.5,
+            },
+            scaleType: 'linear',
+            autoscale: 'local',
+          },
+        ],
+      },
+      {
+        type: 'QuantitativeTrack',
+        trackId: 'BigWig-bin100',
+        name: 'Coverage bin100',
+        assemblyNames: ['asm'],
+        adapter: {
+          type: 'BigWigAdapter',
+          bigWigLocation: {
+            uri: '/data/LESB58/bigwig-bin100.bw',
+            locationType: 'UriLocation',
+          },
+        },
+        displays: [
+          {
+            type: 'LinearWiggleDisplay',
+            displayId: 'BigWig-bin100-LinearWiggleDisplay',
+            height: 150,
+            renderer: {
+              type: 'XYPlotRenderer',
+              color: '#d62728',
+              lineWidth: 2,
+            },
+            scaleType: 'linear',
+            autoscale: 'local',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default myConfig;
