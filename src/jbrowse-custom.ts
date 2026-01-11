d66099c52840ad0f582f825748732e18850f8bb8
goldentree1 <goldentreedev@gmail.com>
2026-01-11 19:35:15 +1300

    refactor

--- src/jbrowse-custom.ts
+++ src/jbrowse-custom.ts
@@ -4,6 +4,137 @@ import { applyPatch } from 'mobx-state-tree';
 import { createViewState } from '@jbrowse/react-linear-genome-view2';
 import Plugin from '@jbrowse/core/Plugin';
 
+/** My custom plugin for JBrowse (allows more customisation) */
+export default class MyJbrowsePlugin extends Plugin {
+  name = 'MyJbrowsePlugin';
+
+  install(pluginManager: PluginManager) {
+    console.log('[MyJbrowsePlugin] installed');
+
+    // example: remove track menu for GFF3 FeatureTracks
+    pluginManager.addToExtensionPoint('TrackMenuItems', (items, ctx) => {
+      const model = ctx?.model;
+
+      if (
+        /** @ts-expect-error fk typescript */
+        model?.configuration?.trackType === 'FeatureTrack' &&
+        /** @ts-expect-error fk typescript */
+        model?.adapterConfig?.type === 'Gff3TabixAdapter'
+      ) {
+        return []; // meant to remove ... menu but doesnt work
+      }
+
+      pluginManager.addToExtensionPoint('FeatureMenuItems', (items, ctx) => {
+        /** @ts-expect-error fk typescript */
+        if (ctx?.track?.adapterConfig?.type === 'Gff3TabixAdapter') {
+          return [];
+        }
+        return items;
+      });
+
+      return items;
+    });
+  }
+}
+
+/** JBrowse config with my custom styling + plugins */
+const jbrowseCustomisations: Omit<JBrowseConfig, 'assembly' | 'tracks'> = {
+  configuration: {
+    // custom styling
+    theme: {
+      palette: {
+        primary: {
+          main: '#155B8E',
+        },
+        secondary: {
+          main: '#155B8E',
+        },
+        tertiary: {
+          main: '#e58703',
+        },
+        quaternary: {
+          main: '#22cb04',
+        },
+      },
+      typography: { fontSize: 12 },
+      spacing: 5,
+      logoPath: {
+        uri: '',
+      },
+    },
+
+    // allow multi-threading (worker)
+    rpc: {
+      defaultDriver: 'WebWorkerRpcDriver',
+    },
+  },
+
+  // allow multi-threading (worker)
+  makeWorkerInstance: () => {
+    return new Worker(new URL('./rpcWorker', import.meta.url), {
+      type: 'module',
+    });
+  },
+
+  // add plugins
+  plugins: [MyJbrowsePlugin],
+};
+
+/** JBrowse's createViewState() ... but with my customisations */
+export function myCreateViewState(config: JBrowseConfig): ViewModel {
+  // overwrite conf with customisations
+  const newConf = {
+    ...config,
+    ...jbrowseCustomisations,
+  };
+
+  // track patches
+  for (const track of newConf.tracks || []) {
+    // add reverse/forward strand colouring to genes (GFF3 track)
+    if (
+      track.type === 'FeatureTrack' &&
+      track?.adapter?.type === 'Gff3TabixAdapter'
+    ) {
+      track.displays = [
+        {
+          type: 'LinearBasicDisplay',
+          displayId: 'GFF3GeneTrack-LinearBasicDisplay',
+          renderer: {
+            type: 'SvgFeatureRenderer',
+            color1: "jexl:get(feature,'strand')>0?'#00FF00':'#ff0000'",
+            color2: "jexl:get(feature,'strand')>0?'#029f02':'#af0101'",
+          },
+        },
+      ];
+    }
+  }
+
+  // edit default session settings
+  if (newConf.defaultSession?.view) {
+    newConf.defaultSession.view.trackLabels = 'overlapping';
+    newConf.defaultSession.view.colorByCDS = false;
+  }
+
+  // build the views
+  const state = createViewState({ ...newConf });
+
+  const view = state.session.views[0];
+  if (!view || !view.tracks) return state;
+  for (const viewTrack of view.tracks) {
+    // multiwiggle tracks should have overlapping lines
+    for (const display of viewTrack.displays || []) {
+      if (display.type === 'MultiLinearWiggleDisplay') {
+        applyPatch(display, [
+          { op: 'replace', path: '/rendererTypeNameState', value: 'multiline' },
+        ]);
+      }
+    }
+  }
+
+  return state satisfies ViewModel;
+}
+
+/** Build a JBrowse-compatible config from my simple config builder */
 export function buildConfig({
   firstRegion,
   ncbiName,
@@ -123,135 +254,4 @@ export function buildConfig({
   }
 
   return conf;
-}
-
-export default class MyJbrowsePlugin extends Plugin {
-  name = 'MyJbrowsePlugin';
-
-  install(pluginManager: PluginManager) {
-    // // sanity check: proves plugin is loaded
-    console.log('[MyJbrowsePlugin] installed');
-
-    // example: remove track menu for GFF3 FeatureTracks
-    pluginManager.addToExtensionPoint('TrackMenuItems', (items, ctx) => {
-      const model = ctx?.model;
-
-      if (
-        /** @ts-expect-error fuck typescript */
-        model?.configuration?.trackType === 'FeatureTrack' &&
-        /** @ts-expect-error fuck typescript #2 */
-        model?.adapterConfig?.type === 'Gff3TabixAdapter'
-      ) {
-        return []; // meant to remove ... menu but doesnt work
-      }
-
-      pluginManager.addToExtensionPoint('FeatureMenuItems', (items, ctx) => {
-        /** @ts-expect-error fuck typescript */
-        if (ctx?.track?.adapterConfig?.type === 'Gff3TabixAdapter') {
-          return [];
-        }
-        return items;
-      });
-
-      return items;
-    });
-  }
-}
-
-const jbrowseCustomisations: Omit<JBrowseConfig, 'assembly' | 'tracks'> = {
-  configuration: {
-    // custom styling
-    theme: {
-      palette: {
-        primary: {
-          main: '#155B8E',
-        },
-        secondary: {
-          main: '#155B8E',
-        },
-        tertiary: {
-          main: '#e58703',
-        },
-        quaternary: {
-          main: '#22cb04',
-        },
-      },
-      typography: { fontSize: 12 },
-      spacing: 5,
-      logoPath: {
-        uri: '',
-      },
-    },
-
-    // allow multi-threading (worker)
-    rpc: {
-      defaultDriver: 'WebWorkerRpcDriver',
-    },
-  },
-
-  // allow multi-threading (worker)
-  makeWorkerInstance: () => {
-    return new Worker(new URL('./rpcWorker', import.meta.url), {
-      type: 'module',
-    });
-  },
-
-  // add plugins
-  plugins: [MyJbrowsePlugin],
-};
-
-/**
- * JBrowse's createViewState() ... but with my customisations.
- */
-export function myCreateViewState(config: JBrowseConfig): ViewModel {
-  // overwrite conf with customisations
-  const newConf = {
-    ...config,
-    ...jbrowseCustomisations,
-  };
-
-  // track patches
-  for (const track of newConf.tracks || []) {
-    // add reverse/forward strand colouring to genes (GFF3 track)
-    if (
-      track.type === 'FeatureTrack' &&
-      track?.adapter?.type === 'Gff3TabixAdapter'
-    ) {
-      track.displays = [
-        {
-          type: 'LinearBasicDisplay',
-          displayId: 'GFF3GeneTrack-LinearBasicDisplay',
-          renderer: {
-            type: 'SvgFeatureRenderer',
-            color1: "jexl:get(feature,'strand')>0?'#00FF00':'#ff0000'",
-            color2: "jexl:get(feature,'strand')>0?'#029f02':'#af0101'",
-          },
-        },
-      ];
-    }
-  }
-
-  // edit default session settings
-  if (newConf.defaultSession?.view) {
-    newConf.defaultSession.view.trackLabels = 'overlapping';
-    newConf.defaultSession.view.colorByCDS = false;
-  }
-
-  // build the views
-  const state = createViewState({ ...newConf });
-
-  const view = state.session.views[0];
-  if (!view || !view.tracks) return state;
-  for (const viewTrack of view.tracks) {
-    // multiwiggle tracks should have overlapping lines
-    for (const display of viewTrack.displays || []) {
-      if (display.type === 'MultiLinearWiggleDisplay') {
-        applyPatch(display, [
-          { op: 'replace', path: '/rendererTypeNameState', value: 'multiline' },
-        ]);
-      }
-    }
-  }
-
-  return state satisfies ViewModel;
 }
